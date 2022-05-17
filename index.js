d82bbf2063fb13eda8b50bfd1fd1146c33b0a983
Malza0408 <parkjh04080@gmail.com>
2022-05-18 02:36:34 +0900

    test send

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('kruger 서버입니다.');
+});
+
 app.listen(PORT, () => {
     console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
