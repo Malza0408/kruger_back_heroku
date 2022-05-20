3734d5c8a074136a03b21897c81bc0bd2be2205c
Malza0408 <parkjh04080@gmail.com>
2022-05-21 00:18:06 +0900

    test ci/cd

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('kruger 서버입니다.');
+  res.send('kruger 서버입니다. CI/CD가 적용 되었는지 테스트!');
 });
 
 app.listen(PORT, () => {
-    console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
